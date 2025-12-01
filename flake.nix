5bc5e2a8fd4b6cce16dccc02aafd3567f8c14e56
Niklas Mohrin <dev@niklasmohrin.de>
2025-12-01 13:35:43 +0100

    Init

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "AOC 2025";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, systems }@inputs:
+    let
+      systems = import inputs.systems;
+      lib = nixpkgs.lib;
+      forAllSystems = lib.genAttrs systems;
+      pkgsFor = lib.genAttrs systems (system: import nixpkgs { inherit system; });
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = pkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              zig
+              zls
+            ];
+          };
+        }
+      );
+    };
+}
